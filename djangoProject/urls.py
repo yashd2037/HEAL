a1c38332f2b4d97e00dfd0bc30db1b35e43b225e
LAPTOP-8AC1VBHG\Haresh <gamer2037@gmail.com>
2021-02-22 04:52:51 -0500

    Survey app creation

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from HEALSurvey import views
 
 urlpatterns = [
+    path('surveys/', include('surveys.urls')),
     path('admin/', admin.site.urls),
-    path('', views.home, name="home"),
+    path('home/', views.home, name="home"),
     path('topic/', views.topic, name='topic'),
     path('survey/', views.survey, name='survey'),
     path('team/', views.team, name='team')
