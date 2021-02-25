a8a614e76628e08a2a5d62ec92dcf6e087203cc9
nickweb99 <nickweb99@outlook.com>
2021-02-25 14:41:14 -0500

    Nick Commit 4

--- HEALSurvey/models.py
+++ HEALSurvey/models.py
@@ -9,9 +9,25 @@ TOPIC_CHOICES = (
     ('Employment', 'Employment')
 )
 
+
 class Topics(models.Model):
     topic = models.CharField("First Topic", max_length=15, blank=True, choices=TOPIC_CHOICES)
     topic2 = models.CharField("Second Topic", max_length=15, blank=True, choices=TOPIC_CHOICES)
 
     def __str__(self):
         return self.topic
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.choice_text
--- HEALSurvey/views.py
+++ HEALSurvey/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import TopicForm
 from .models import Topics
+from django.http import HttpResponse
+from .models import Question
 
 def home(request):
     return render(request, 'IntroPage.html')
@@ -19,4 +21,21 @@ def survey(request):
 def team(request):
     return render(request, 'Team.html')
 
+from django.shortcuts import render, get_object_or_404
 
+
+def index(request):
+    question_list = Question.objects.all()
+    context = {'question_list': question_list}
+    return render(request, 'index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
