62c592f35d40f0cd660d7a89a24c37375082d10b
lilyphan17 <lilyphan17@yahoo.com>
2021-03-08 18:06:14 -0500

    vid

--- HEALSurvey/views.py
+++ HEALSurvey/views.py
@@ -1,15 +1,40 @@
-from django.shortcuts import render, redirect
-from .forms import TopicForm
-from .models import Topics
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import TopicForm, QuestionForm
+from django.http import HttpResponse
+from .models import Topics, Question
 
-def home(request):
+
+def home(request):  # Renders home page
     return render(request, 'IntroPage.html')
 
-def topic(request):
-    form = TopicForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        return redirect('')
-    context = {'form': form}
-    return render(request, 'TopicPage.html', context)
 
+def topic(request):  # Checks the validity of the form and saves if valid, otherwise it renders the topic page
+    form = TopicForm(request.POST or None)
+    question = Question.objects.get(id=1)  # Gets question at id 1, temporary solution
+    if form.is_valid():  # Checks is form received proper input
+        form.save()
+        return render(request, 'Survey.html', {'question': question})  # Renders survey with current question
+    return render(request, 'TopicPage.html', {'form': form})  # Re-renders the current page
+
+
+def team(request):  # renders team page
+    return render(request, 'Team.html')
+
+
+def index(request):  # renders a list of all questions in the database on index page
+    question_list = Question.objects.all()
+    context = {'question_list': question_list}
+    return render(request, 'index.html', context)
+
+
+def survey(request, question_id):  # renders initial survey page with selected question object from database
+    baseQuestion = Question.objects.get(id=question_id)  # Gets the question at the current id
+    if request.method == 'POST':
+        question = Question.objects.get(id=baseQuestion.NextIDA)  # Gets the question at the next A branch ID
+        return render(request, 'Survey.html', {'question': question})
+    question = baseQuestion
+    return render(request, 'Survey.html', {'question': question})
+
+
+def video(request):  # renders video page
+    return render(request, 'Video.html')
--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from HEALSurvey import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
-    path('topic/', views.topic, name='topic')
+    path('topic/', views.topic, name='topic'),
+    path('team/', views.team, name='team'),
+    path('index/', views.index, name='index'),
+    path('<int:question_id>/', views.survey, name='survey'),
+    path('video/', views.video, name='video')
 ]
