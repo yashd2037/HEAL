66b69178f8ad9293fff11d59ba98ea6dcb281b16
lilyphan17 <lilyphan17@yahoo.com>
2021-03-08 18:03:16 -0500

    vid

--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!C:\Users\Nicholas\PycharmProjects\djangoProject\venv\Scripts\python.exe
+#!C:\Users\Ly\PycharmProjects\djangoProject\venv\Scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
