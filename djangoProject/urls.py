823750dd8d4f7914164ce8eefa2b3954f74d8e85
nickweb99 <nickweb99@outlook.com>
2021-03-14 17:05:42 -0400

    Nick Sprint 3 Commit 3

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('team/', views.team, name='team'),
     path('index/', views.index, name='index'),
     path('<int:question_id>/', views.survey, name='survey'),
+    path('login/', views.login_view, name="login"),
+    path('logout/', views.logout_view, name="logout"),
+    path('register/', views.register_view, name="register"),
     path('video/', views.video, name='video')
 ]
