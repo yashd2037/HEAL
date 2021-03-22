5eb91a69f8e538cf6feda31f9c6be0236af39050
LAPTOP-8AC1VBHG\Haresh <gamer2037@gmail.com>
2021-03-22 11:51:56 -0400

    Changes for 3/22

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('login/', views.login_view, name="login"),
     path('logout/', views.logout_view, name="logout"),
     path('register/', views.register_view, name="register"),
-    path('video/', views.video, name='video')
+    path('video/', views.video, name='video'),
+
 ]
