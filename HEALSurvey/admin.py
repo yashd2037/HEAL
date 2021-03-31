ae41744b38b5b9a3cc9ea6b176efcf6aa61d0491
nickweb99 <nickweb99@outlook.com>
2021-03-31 16:49:09 -0400

    Nick Sprint 4 Commit 1.5 (FIX)

--- HEALSurvey/admin.py
+++ HEALSurvey/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Question, Choice, SummaryStatement, SummaryChoice
+from .models import Question, Choice, SummaryStatement
 
 
 class ChoiceInline(admin.StackedInline):
@@ -15,4 +15,4 @@ class QuestionAdmin(admin.ModelAdmin):
 admin.site.register(Question, QuestionAdmin)
 
 admin.site.register(SummaryStatement)
-admin.site.register(SummaryChoice)
+
